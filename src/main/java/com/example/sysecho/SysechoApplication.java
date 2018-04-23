fa3dc9baf0a60561e7efb9979140d023ce3f630b
sysecho <707406343@qq.com>
2018-04-23 10:00:20 +0800

    去掉不需要自动配置的项

--- src/main/java/com/example/sysecho/SysechoApplication.java
+++ src/main/java/com/example/sysecho/SysechoApplication.java
@@ -2,8 +2,11 @@ package com.example.sysecho;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { HibernateJpaAutoConfiguration.class })
 public class SysechoApplication {
 
 	public static void main(String[] args) {
