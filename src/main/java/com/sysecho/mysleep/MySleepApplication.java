247ec4fc8e1c4752d0feff1767a7f2b25a1d5ec7
sysecho <707406343@qq.com>
2018-09-30 14:04:47 +0800

    去掉不使用的包

--- src/main/java/com/sysecho/mysleep/MySleepApplication.java
+++ src/main/java/com/sysecho/mysleep/MySleepApplication.java
@@ -2,8 +2,6 @@ package com.sysecho.mysleep;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication(exclude = { HibernateJpaAutoConfiguration.class })
